f1a308b3a809e6b27b13d905c99f11a088b019f2
Yoftil7 <yoftil7@gmail.com>
2025-09-05 17:06:15 +0300

    feat: Add generic error and pagination tests

--- tests/test_task.py
+++ tests/test_task.py
@@ -66,3 +66,67 @@ def test_list_tasks_empty(client):
     assert res.status_code == 200
     data = res.get_json()
     assert data["items"] == []
+
+
+# pagination test
+
+
+def test_pagiantion_first_page(client, add_tasks):
+    add_tasks(15)
+    res = client.get("/tasks?page=1&per_page=10")
+    assert res.status_code == 200
+    data = res.get_json()
+
+    assert data["meta"]["page"] == 1
+    assert data["meta"]["per_page"] == 10
+    assert data["meta"]["total"] == 15
+    assert data["meta"]["pages"] == 2
+    assert len(data["items"]) == 10
+    assert data["meta"]["has_next"] is True
+    assert data["meta"]["has_prev"] is False
+
+
+def test_pagination_second_page(client, add_tasks):
+    add_tasks(15)
+    res = client.get("/tasks?page=2&per_page=10")
+    assert res.status_code == 200
+    data = res.get_json()
+
+    assert data["meta"]["page"] == 2
+    assert len(data["items"]) == 5
+    assert data["meta"]["has_next"] is False
+    assert data["meta"]["has_prev"] is True
+
+
+def test_pagination_invalid_params(client):
+    res = client.get("/tasks?page=0&per_page=200")
+    assert res.status_code == 400
+    data = res.get_json()
+    assert "page" in data["error"]["details"]
+    assert "per_page" in data["error"]["details"]
+
+
+def test_http_exception_handler(client):
+    # trigger 404
+    res = client.get("/none-existing")
+    assert res.status_code == 404
+    data = res.get_json()
+    assert "error" in data
+    assert data["error"]["type"] == "NotFound"
+
+
+def test_generic_exception_handler(app, client, monkeypatch, add_task):
+    add_task(description="Test task")
+
+    # Force a crash by monkeypatching the serializer's dump method
+    def boom(*args, **kwargs):
+        raise RuntimeError("Boom!")
+
+    from app import schemas
+
+    monkeypatch.setattr(schemas.TaskSchema, "dump", boom)
+
+    res = client.get("/tasks/1")
+    assert res.status_code == 500
+    data = res.get_json()
+    assert data["error"]["type"] == "InternalServerError"
